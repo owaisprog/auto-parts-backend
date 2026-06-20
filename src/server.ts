902680b85a9d260cec07788546e30e92fa4bcd3d
ChMubasharAli <muhammadmubashirali075@gmail.com>
2026-06-21 02:29:15 +0500

    update changes

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,10 @@
 import app from "./app";
-// import { env } from "./config/env";
+import { env } from "./config/env";
 
-// const PORT = env.PORT;
+const PORT = env.PORT;
 
-// app.listen(PORT, () => {
-//   console.log(`🚀 Server running on http://localhost:${PORT}`);
-//   console.log(`📍 Environment: ${env.NODE_ENV}`);
-//   console.log(`🔗 Frontend URL: ${env.FRONTEND_URL}`);
-// });
-
-export default app;
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on http://localhost:${PORT}`);
+  console.log(`📍 Environment: ${env.NODE_ENV}`);
+  console.log(`🔗 Frontend URL: ${env.FRONTEND_URL}`);
+});
