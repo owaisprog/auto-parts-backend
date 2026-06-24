57fa3a43ecbe936afd96fc2bef3687e93d555bf4
ChMubasharAli <muhammadmubashirali075@gmail.com>
2026-06-24 18:53:45 +0500

    update changes

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,8 +1,9 @@
 import "dotenv/config";
 import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "../../generated/prisma/client";
+import { env } from "./env";
 
-const connectionString = `${process.env.DATABASE_URL}`;
+const connectionString = `postgresql://${env.DB_USER}:${env.DB_PASSWORD}@${env.DB_HOST}:${env.DB_PORT}/${env.DB_NAME}?sslmode=require`;
 
 const adapter = new PrismaPg({ connectionString });
 const prisma = new PrismaClient({ adapter });
--- src/config/env.ts
+++ src/config/env.ts
@@ -5,7 +5,12 @@ dotenv.config();
 export const env = {
   PORT: process.env.PORT || "5000",
   NODE_ENV: process.env.NODE_ENV || "development",
-  DATABASE_URL: process.env.DATABASE_URL || "",
+  DB_HOST: process.env.DB_HOST || "",
+  DB_PORT: process.env.DB_PORT || "",
+  DB_NAME: process.env.DB_NAME || "",
+  DB_USER: process.env.DB_USER || "",
+  DB_PASSWORD: process.env.DB_PASSWORD || "",
+
   ADMIN_EMAIL: process.env.ADMIN_EMAIL || "kenelson1909p@yahoo.com.sg",
   ADMIN_PASSWORD: process.env.ADMIN_PASSWORD || "",
   JWT_SECRET:
@@ -21,7 +26,13 @@ export const env = {
   FRONTEND_URL: process.env.FRONTEND_URL || "http://localhost:5173",
 } as const;
 
-if (!env.DATABASE_URL) {
+if (
+  !env.DB_HOST &&
+  !env.DB_USER &&
+  !env.DB_NAME &&
+  !env.DB_PASSWORD &&
+  !env.DB_PORT
+) {
   throw new Error("DATABASE_URL is required");
 }
 
